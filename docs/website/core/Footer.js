b0bbcd61a232fbe662974dedb14d815b93281a05
Miguel Mota <miguelmota2@gmail.com>
2019-04-02 23:43:03 -0700

    [docs] add mpv logo, change color

--- docs/website/core/Footer.js
+++ docs/website/core/Footer.js
@@ -49,15 +49,6 @@ class Footer extends React.Component {
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
-            <a
-              href="http://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
             <a href="https://discordapp.com/">Project Chat</a>
             <a
               href="https://twitter.com/"
@@ -83,18 +74,6 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
-          href="https://opensource.facebook.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
